5f36171733980604e999059da0c8b0d78ee49bca
sidprs <sidprasad2004@gmail.com>
2025-11-10 00:06:28 -0700

    some c++ stuff

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,8 +1,38 @@
 #include <iostream>
-
-using namespace std; 
+#include <vector>
   
+
+
+class RingBuffer{
+
+  private:
+   int head_; 
+   int tail_; 
+   int capacity_;
+   int size_;
+   int* array_;
+
+  public:
+    RingBuffer(int capacity_) : size_(0), head_(0), tail_(0){
+      array_ = new int [capacity_];
+
+    }
+    ~RingBuffer();
+    RingBuffer(const RingBuffer&) = default;
+    RingBuffer(RingBuffer&&);
+      
+    RingBuffer operator=(const RingBuffer&);
+    RingBuffer operator=( RingBuffer&&);
+
+
+}
+
+
+
 int main(){
 
+  
+
   return 0;
+
 }
